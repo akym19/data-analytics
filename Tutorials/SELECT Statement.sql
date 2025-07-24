a534bd164588f34b510058975f9161316eaa4f50
Raffy Jay Dayag <110025185+akym19@users.noreply.github.com>
2025-07-25 04:05:35 +0800

    Delete Tutorials directory

--- Tutorials/SELECT Statement.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM employee_demographics;
-
-SELECT first_name, last_name, birth_date, age, age+10 AS added_age
-FROM employee_demographics;
-
-SELECT DISTINCT gender
-FROM employee_demographics;
